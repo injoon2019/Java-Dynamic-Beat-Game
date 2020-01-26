91a88f0d2ed83a0415ee5ee8ece702603aa522c6
Injun Son <injoon2019@gmail.com>
2020-01-26 15:50:51 -0800

    14 make notes with rythm

--- src/dynamic_beat_14/Main.java
+++ src/dynamic_beat_14/Main.java
@@ -1,15 +1,20 @@
-package dynamic_beat_8;
+package dynamic_beat_14;
 
 public class Main {
 
 	public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
-	public static final int REACH_TIME = 0;
+	public static final int NOTE_SPEED = 3;
+	public static final int SLEEP_TIME = 10;
+	public static final int REACH_TIME = 2;
+	
 	
 	public static void main(String[] args) {
 		
 		new DynamicBeat();
 
 	}
+	
+	
 
 }
